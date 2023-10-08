14a7430372dda9c68085d9851fd5bdc8bc8be015
peanutmutton <peanutmutton@outlook.com>
2023-10-08 14:34:11 +0600

    shop with basic product form

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import ProductFormView
+
 
 urlpatterns = [
+    path('', ProductFormView.as_view(), name="product_form"),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .models import Product
+from .forms import FileFieldForm
 
-# Create your views here.
+
+class ProductFormView(FormView):
+    form_class = FileFieldForm
+    template_name = "product_form.html"
+    success_url = "success"
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        files = form.cleaned_data["file_field"]
+        product = Product.objects.create(
+            title = form.cleaned_data['title'],
+            description= form.cleaned_data['description'],
+        )
+        for f in files:
+            product.image_set.create(image=f)
+        return super().form_valid(form)
